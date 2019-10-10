664ecff8f860cc67221d9a2790af87787f3db676
don-garu <hwangji9637@gmail.com>
2019-10-10 14:58:19 +0900

    Edit

--- src/read_mail.py
+++ src/read_mail.py
@@ -5,7 +5,7 @@ import base64
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import requests
+from google.auth.transport.requests import Request
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
@@ -41,7 +41,7 @@ def main():
             for test in msg['payload']['headers']:
                 if test['name'] == "Subject":
                     print(test)
-            print(msg['payload']['body'])
+            print(msg['payload']['body'][0])
 #            print(msg['payload']['headers'][0])
 #            print(msg['payload']['headers'][1])
 #            print(msg['payload']['headers'][3])
